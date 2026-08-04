6715292b52355becea5c2a92dfc009236b544ff7
Kuldeep Katre <kuldeepkatre2693@gmail.com>
2026-08-04 15:43:34 +0530

    Rename Models to models

--- server/Models/watchParty.js
+++ server/Models/watchParty.js
@@ -38,7 +38,7 @@ const watchPartySchema = new mongoose.Schema(
 );
 
 const WatchParty =
-  mongoose.Models.watchParty ||
+  mongoose.models.watchParty ||
   mongoose.model("watchParty", watchPartySchema);
 
 export default WatchParty;
